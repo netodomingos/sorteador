f42ff5ad85494741b34acc85d43e2418f61d2eb4
geraldo_domingos <geraldoneto@locus.software>
2023-02-20 14:25:26 -0300

    ✨feature: configuracao screen

---
+++ src/pages/Configuracao.tsx
@@ -0,0 +1,13 @@
+import Formulario from '../components/Formulario/Formulario'
+import ListaParticipantes from '../components/ListaParticipantes/ListaParticipantes'
+import Rodape from '../components/Rodape/Rodape'
+
+export default function Configuracao() {
+  return (
+    <>
+        <Formulario/>
+        <ListaParticipantes />
+        <Rodape />
+    </>
+  )
+}
