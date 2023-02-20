7e01f8c59174d9d6f4bf96dbcfb69364d137b364
geraldo_domingos <geraldoneto@locus.software>
2023-02-20 12:26:51 -0300

    ✨feature: add participants hook in form component

--- src/components/Formulario/Formulario.tsx
+++ src/components/Formulario/Formulario.tsx
@@ -1,12 +1,40 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useState } from 'react'
+import { useAdicionarParticipante } from '../../state/hooks/useAdcionarParticipante'
+import { useMensagemDeErro } from '../../state/hooks/useMensagemDeErro'
 
 export default function Formulario() {
+  const [nome, setNome] = useState('')
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const adicionarNaLista = useAdicionarParticipante()
+  const mensagemDeErro = useMensagemDeErro()
+
+  const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    adicionarNaLista(nome)
+
+    setNome('')
+    inputRef.current?.focus()
+
+  }
+
   return (
-    <form>
-        <input type="text" placeholder='Insira os nomes dos participantes'/>
-        <button disabled={true}>
-            Adicionar
+    <form onSubmit={adicionarParticipante}>
+        <input 
+          type="text" 
+          placeholder='Insira os nomes dos participantes'
+          value={nome}
+          onChange={evento => setNome(evento.target.value)}
+          ref={inputRef}
+        />
+
+        <button disabled={!nome}>
+          Adicionar
         </button>
+        
+        {mensagemDeErro && <p role='alert'>{mensagemDeErro}</p>}
     </form>
   )
 }
