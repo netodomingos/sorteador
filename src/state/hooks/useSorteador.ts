507912029dd4c98bb2b516609fb46938bc49a78a
geraldo_domingos <geraldoneto@locus.software>
2023-02-21 10:51:45 -0300

    ✨feature: refact hook useSorteador

--- src/state/hooks/useSorteador.ts
+++ src/state/hooks/useSorteador.ts
@@ -1,22 +1,14 @@
-import shuffle from "just-shuffle"
 import { useSetRecoilState } from 'recoil'
+import { realizarSorteio } from "../../utils/RealizarSorteio"
 import { resultadoState } from "../atom"
 import { useListaDeParticipantes } from "./useListaDeParticipantes"
 
 export const useSorteador = () => {
     const participantes = useListaDeParticipantes()
     const setResultado = useSetRecoilState(resultadoState)
-
+    
     return () => {
-        const totalParticipantes = participantes.length
-        const embaralhado = shuffle(participantes)
-        const resultado = new Map<string, string>()
-
-        for (let index = 0; index < totalParticipantes; index++) {
-            const indiceAmigo = index === (totalParticipantes - 1) ? 0 : index
-            resultado.set(embaralhado[index], embaralhado[indiceAmigo])
-        }
-
+        const resultado = realizarSorteio(participantes)
         setResultado(resultado)
     }
 }
