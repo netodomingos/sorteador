588e6623da67729791523179b8cae3f38cfc7dd7
geraldo_domingos <geraldoneto@locus.software>
2023-02-21 10:51:29 -0300

    ✨feature: sort function & test

---
+++ src/tests/utils/RealizarSorteio.test.ts
@@ -0,0 +1,15 @@
+import React from 'react'
+import { realizarSorteio } from '../../utils/RealizarSorteio'
+
+describe('O sorteador de um amigo secreto', () => {
+    test('Cada participante não sorteie o próprio nome', () => { 
+        const participantes = [ 'Geraldo', 'Luana', 'Marcelo', 'Carlos', 'Lucas', 'Maria', 'Neto' ]   
+        
+        const sorteio = realizarSorteio(participantes)
+
+        participantes.forEach(participante => {
+            const amigoSecreto = sorteio.get(participante)
+            expect(amigoSecreto).not.toEqual(participante)
+        })
+    })
+})
