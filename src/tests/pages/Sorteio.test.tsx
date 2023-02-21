00e46448177175315a5c4911260387aa2500581a
geraldo_domingos <geraldoneto@locus.software>
2023-02-21 13:57:38 -0300

    test run CI

--- src/tests/pages/Sorteio.test.tsx
+++ src/tests/pages/Sorteio.test.tsx
@@ -39,6 +39,7 @@ describe('Na página de sorteio', () => {
         const opcoes = screen.queryAllByRole('option')
 
         expect(opcoes).toHaveLength(participantes.length + 1)
+        
     })
 
     test('O amigo secreto é exibido quando solicitado', () => {
