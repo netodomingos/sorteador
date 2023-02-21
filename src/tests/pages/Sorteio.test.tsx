f7bd1740a052b20407ed35d268b7e22a7770bd1d
geraldo_domingos <geraldoneto@locus.software>
2023-02-21 13:45:48 -0300

    ✨feature: adjust length test

--- src/tests/pages/Sorteio.test.tsx
+++ src/tests/pages/Sorteio.test.tsx
@@ -38,7 +38,7 @@ describe('Na página de sorteio', () => {
 
         const opcoes = screen.queryAllByRole('option')
 
-        expect(opcoes).toHaveLength(participantes.length)
+        expect(opcoes).toHaveLength(participantes.length + 1)
     })
 
     test('O amigo secreto é exibido quando solicitado', () => {
